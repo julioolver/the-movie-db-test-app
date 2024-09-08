5cdb7376337213b8f2f8554a9fe7fd079865bbf2
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-08 18:27:14 -0300

    feat: auth #4 - adjusted to disable app-bar in login page, and change name component blankLayout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar>
+    <v-app-bar v-if="!isLoginPage">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-app-bar-title>Movie List</v-app-bar-title>
@@ -19,7 +19,8 @@
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary>
+    <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary
+      v-if="!isLoginPage">
       <v-list :items="items"></v-list>
     </v-navigation-drawer>
     <v-main>
@@ -29,8 +30,10 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
 const drawer = ref(false)
 
 const items = [
@@ -51,4 +54,8 @@ const items = [
     value: 'buzz',
   },
 ]
+
+const isLoginPage = computed(() => {
+  return router.currentRoute.value.path === "/login";
+})
 </script>
