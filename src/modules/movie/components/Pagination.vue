02c97cde6a3388d3dedb5dd4a63895fa69b04150
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-08 13:20:56 -0300

    feat: movie-list #1 - created page component

---
+++ src/modules/movie/components/Pagination.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-pagination v-model="currentPage" :length="totalPages" @input="$emit('page-changed', currentPage)"></v-pagination>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, ref, watch } from 'vue'
+
+const props = defineProps<{
+  currentPage: number;
+  totalPages: number;
+}>()
+
+const currentPage = ref(props.currentPage)
+
+const emit = defineEmits<{
+  (e: 'page-changed', newPage: number): void;
+}>()
+
+watch(() => props.currentPage, (newPage) => {
+  emit('page-changed', newPage)
+})
+</script>
