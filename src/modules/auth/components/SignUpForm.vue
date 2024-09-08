6f6dd66b41d4583ba40eb41b54d427164ffe4053
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-08 18:28:12 -0300

    feat: auth #4 - created component forms to login and index page with sign in/sign up tabs

---
+++ src/modules/auth/components/SignUpForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-form ref="registerForm" lazy-validation>
+    <v-text-field v-model="controller.user.name" label="Nome" :rules="[v => !!v || 'Nome é obrigatório']" required
+      hide-details="auto"></v-text-field>
+
+    <v-text-field v-model="controller.user.email" label="Email" type="email"
+      :rules="[v => !!v || 'Email é obrigatório', v => /.+@.+\..+/.test(v) || 'Email inválido']" required
+      hide-details="auto"></v-text-field>
+
+    <v-text-field v-model="controller.user.password" label="Senha" type="password"
+      :rules="[v => !!v || 'Senha é obrigatória']" required hide-details="auto"></v-text-field>
+
+    <v-text-field v-model="controller.user.password_confirmation" label="Confirmar Senha" type="password"
+      :rules="[v => v === controller.isPasswordMatching.value || 'As senhas não coincidem']" required
+      hide-details="auto"></v-text-field>
+
+    <v-btn color="success" block @click="controller.register()">Cadastrar</v-btn>
+  </v-form>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+import { AuthController } from '../controller/AuthController';
+
+const props = defineProps({
+  controller: {
+    type: Object as PropType<AuthController>,
+    required: true,
+  },
+})
+
+const controller = props.controller
+</script>
