201c0a3ef0d9c440695427c1d667b724f26eb2b9
Julio Cesar Oliveira da Silva <julioghost1@gmail.com>
2024-09-08 12:45:57 -0300

    feat: movie-list #1 - Created axios global config with interceptor to get auth token

---
+++ src/plugins/axios/index.ts
@@ -0,0 +1,24 @@
+import storages from "@/services/storages";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8088/api/",
+  timeout: 1000,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Access: 'application/json',
+  }
+});
+
+instance.interceptors.request.use(config => {
+  const token = storages.getToken()
+
+  if (token) {
+    config.headers.common.Authorization = `Bearer ${token}`
+  }
+
+  return config
+}, error => Promise.reject(error))
+
+export default instance;
